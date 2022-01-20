5d0b9c49786e5bdca4f30cc6804f4343c2162e45
Rodrigo Torres <torresperez12@hotmail.com>
2022-01-19 18:26:02 -0800

    1c

---
+++ root/index.js
@@ -0,0 +1,165 @@
+let planetas = [
+    {
+        nombre: 'Tierra',
+        info: 'Si bien la Tierra es solo el quinto planeta más grande del sistema solar, es el único mundo en nuestro sistema solar con agua líquida en la superficie. Un poco más grande que el cercano Venus, la Tierra es el más grande de los cuatro planetas más cercanos al Sol, todos los cuales están hechos de roca y metal. El nombre Tierra tiene al menos 1.000 años. Todos los planetas, excepto la Tierra, recibieron nombres de dioses y diosas griegos y romanos. Sin embargo, el nombre Tierra es una palabra germánica, que simplemente significa “el suelo”',
+        estructura: 'La estructura interna de la Tierra está formada por capas esféricas concéntricas: una corteza sólida de silicatos exterior, una astenosfera y un manto altamente viscosos, un núcleo externo líquido que es mucho menos viscoso que el manto y un núcleo interno sólido. La comprensión científica de la estructura interna de la Tierra se basa en observaciones de topografía y batimetría, observaciones de rocas en afloramientos, muestras llevadas a la superficie desde mayores profundidades por volcanes o actividad volcánica, análisis de las ondas sísmicas que pasan por la Tierra, mediciones de campos gravitacionales y magnéticos de la Tierra, y experimentos con sólidos cristalinos a presiones y temperaturas características del interior profundo de la Tierra. ',
+        rotacion: '24 hrs',
+        traslacion: '365 dias',
+        radio: '6,371 km',
+        lunas: '1'
+    },
+    {
+        nombre: 'Mercurio',
+        info: 'El planeta más pequeño de nuestro sistema solar y el más cercano al Sol, Mercurio es solo un poco más grande que la Luna de la Tierra. Desde la superficie de Mercurio, el Sol parecería tres veces más grande que visto desde la Tierra, y la luz del sol sería hasta siete veces más brillante. A pesar de su proximidad al Sol, Mercurio no es el planeta más caliente de nuestro sistema solar; ese título pertenece al cercano Venus, gracias a su densa atmósfera. Debido a la órbita elíptica (en forma de huevo) de Mercurio y su lenta rotación, el Sol parece salir brevemente, ponerse y salir de nuevo desde algunas partes de la superficie del planeta. Lo mismo sucede a la inversa al atardecer.',
+        estructura: 'Mercurio es uno de los cuatro planetas rocosos o sólidos; es decir, tiene un cuerpo rocoso, como la Tierra. Este planeta es el más pequeño de los cuatro, con un diámetro de 4879 km en el ecuador. Mercurio está formado aproximadamente por un 70 % de elementos metálicos y un 30 % de silicatos. La densidad de este planeta es la segunda más grande de todo el sistema solar, siendo su valor de 5430 kg/m³, solo un poco menor que la densidad de la Tierra. La densidad de Mercurio se puede usar para deducir los detalles de su estructura interna. Mientras la alta densidad de la Tierra se explica considerablemente por la compresión gravitacional, particularmente en el núcleo, Mercurio es mucho más pequeño y sus regiones interiores no están tan comprimidas. Por tanto, para explicar esta gran densidad, el núcleo debe ocupar gran parte del planeta y además ser rico en hierro,​ material con una alta densidad.​ Los geólogos estiman que el núcleo de Mercurio ocupa un 42 % de su volumen total (el núcleo de la Tierra apenas ocupa un 17 %). Este núcleo estaría parcialmente fundido,​ lo que explicaría el campo magnético del planeta.  ',
+        rotacion: '58 dias',
+        traslacion: '88 dias',
+        radio: '2,439.7 km',
+        lunas: '0'
+    },
+    {
+        nombre: 'Venus',
+        info: 'Venus es el segundo planeta desde el Sol y es el vecino planetario más cercano a la Tierra. Es uno de los cuatro planetas interiores, terrestres (o rocosos), y a menudo se le llama el gemelo de la Tierra porque es similar en tamaño y densidad. Sin embargo, estos no son gemelos idénticos: existen diferencias radicales entre los dos mundos. Venus tiene una atmósfera espesa y tóxica llena de dióxido de carbono y está perpetuamente envuelta en espesas nubes amarillentas de ácido sulfúrico que atrapan el calor y provocan un efecto invernadero descontrolado. Es el planeta más caliente de nuestro sistema solar, aunque Mercurio está más cerca del Sol. Las temperaturas de la superficie de Venus son de unos 900 grados Fahrenheit (475 grados Celsius), lo suficientemente calientes como para derretir el plomo. La superficie es de un color oxidado y está salpicada de montañas intensamente trituradas y miles de grandes volcanes. Los científicos creen que es posible que algunos volcanes todavía estén activos.',
+        estructura: 'Venus es un poco más pequeño que la Tierra, con un diámetro de un 95% del de la Tierra (12,103 km) y una masa de 81% del de la Tierra. Si pudiéramos caminar alrededor de la superficie del planeta sin ser matados por las explosiones tóxicas de la (atmósfera), la gravedad sería parecida a la gravedad en la superficie de la Tierra.',
+        rotacion: '243 dias',
+        traslacion: '225 dias',
+        radio: '6,051.8 km',
+        lunas: '0'
+    },
+    {
+        nombre: 'Marte',
+        info: 'Marte es el cuarto planeta desde el Sol: un mundo polvoriento, frío y desértico con una atmósfera muy delgada. Marte también es un planeta dinámico con estaciones, casquetes polares, cañones, volcanes extintos y evidencia de que fue aún más activo en el pasado. Marte es uno de los cuerpos más explorados de nuestro sistema solar, y es el único planeta al que hemos enviado rovers para recorrer el paisaje alienígena. Actualmente, la NASA tiene dos rovers (Curiosity & Perseverance), un módulo de aterrizaje (InSight) y un helicóptero (Ingenuity) que exploran la superficie de Marte. El rover Perseverance, el rover más grande y avanzado que la NASA ha enviado a otro mundo, aterrizó en Marte el 18 de febrero de 2021, después de un viaje de 203 días atravesando 293 millones de millas (472 millones de kilómetros). El helicóptero Ingenuity viajó a Marte unido al vientre de Perseverance.',
+        estructura: 'Como en la Tierra, Marte es un planeta con diferencias, lo que significa que tiene un núcleo formado por hierro-níquel, un manto, y una corteza de silicatos menos densa. El representativo color rojo del planeta es debido a la cantidad de óxido de hierro existente en su superficie. Basándose en las fuentes de esas bases de datos, los científicos creen que los elementos más abundantes químicos en la corteza de Marte, además de silicio y oxígeno, son: hierro, magnesio, aluminio, calcio, y potasio. Estos elementos son los componentes principales de los minerales que comprenden rocas ígneas, otros elementos como titanio, cromo, manganeso, azufre, fósforo, sodio y cloro son menos abundantes, aun así también son componentes importantes de muchos minerales accesorios​ en rocas y de minerales secundarios (productos de meteorización) en el polvo y suelos (regolito). El hidrógeno está presente en forma de agua (H2O), hielo y en minerales hidratados. También se detectó carbono como dióxido de carbono (CO2) en la atmósfera y a veces en hielo seco en los polos. ',
+        rotacion: '24 hrs',
+        traslacion: '687 dias',
+        radio: '3,389.5 km',
+        lunas: '2'
+    },
+    {
+        nombre: 'Jupiter',
+        info: 'Júpiter tiene una larga historia de científicos sorprendentes, desde 1610 cuando Galileo Galilei encontró las primeras lunas más allá de la Tierra. Ese descubrimiento cambió la forma en que vemos el universo. Quinto en la línea desde el Sol, Júpiter es, con mucho, el planeta más grande del sistema solar: más del doble de masa que todos los demás planetas juntos. Las rayas y remolinos familiares de Júpiter son en realidad nubes frías y ventosas de amoníaco y agua, que flotan en una atmósfera de hidrógeno y helio. La icónica Gran Mancha Roja de Júpiter es una tormenta gigante más grande que la Tierra que se ha desatado durante cientos de años.',
+        estructura: 'Júpiter es un planeta gaseoso y como tal está formado principalmente por Hidrógeno y Helio, aunque se cree que presenta un núcleo rocoso del tamaño de la Tierra. La presencia de capas de conductores líquidos (hidrógeno metálico) dota a Júpiter de campo magnético.',
+        rotacion: '9.48 hrs',
+        traslacion: '12 años',
+        radio: '69,911 km',
+        lunas: '63'
+    },
+    {
+        nombre: 'Saturno',
+        info: 'Saturno es el sexto planeta desde el Sol y el segundo planeta más grande de nuestro sistema solar. Adornado con miles de hermosos rizos, Saturno es único entre los planetas. No es el único planeta que tiene anillos, hechos de trozos de hielo y roca, pero ninguno es tan espectacular o complicado como el de Saturno. Al igual que el gigante gaseoso Júpiter, Saturno es una bola masiva compuesta principalmente de hidrógeno y helio.',
+        estructura: 'Carece de superficie sólida. Está compuesto principalmente por hidrógeno y helio y su interior es posiblemente similar al de Júpiter, con un núcleo denso de roca, hielo, agua y otros compuestos sólidos. Está rodeado de hidrógeno metálico líquido dentro de otra capa de hidrógeno líquido. Su campo magnético es 578 veces más potente que el de la Tierra.',
+        rotacion: '10.7 hrs',
+        traslacion: '12 años',
+        radio: '58,232 km',
+        lunas: '53'
+    },
+    {
+        nombre: 'Urano',
+        info: 'Urano es el séptimo planeta desde el Sol y tiene el tercer diámetro más grande de nuestro sistema solar. Fue el primer planeta encontrado con la ayuda de un telescopio, Urano fue descubierto en 1781 por el astrónomo William Herschel, aunque originalmente pensó que era un cometa o una estrella. Fue dos años más tarde que el objeto fue universalmente aceptado como un nuevo planeta, en parte debido a las observaciones del astrónomo Johann Elert Bode. Herschel intentó sin éxito nombrar su descubrimiento Georgium Sidus en honor al rey Jorge III. En cambio, la comunidad científica aceptó la sugerencia de Bode de nombrarlo Urano, el dios griego del cielo, como sugirió Bode',
+        estructura: 'Está rodeado por 13 anillos. Es un gigante de hielo, en lugar de un gigante de gas. Está compuesto, en su mayor parte, por hielo fluido sobre un núcleo sólido.Tiene una atmósfera densa hecha de metano, hidrógeno y helio. Es el único planeta que rota de lado. Da vueltas en la dirección contraria a las de la Tierra y de la mayoría de los planetas',
+        rotacion: '17 hrs',
+        traslacion: '84 años',
+        radio: '25,362 km',
+        lunas: '27'
+    },
+    {
+        nombre: 'Neptuno',
+        info: 'Oscuro, frío y azotado por vientos supersónicos, el gigante de hielo Neptuno es el octavo y más distante planeta de nuestro sistema solar. Más de 30 veces más lejos del Sol que la Tierra, Neptuno es el único planeta de nuestro sistema solar que no es visible a simple vista y el primero predicho por las matemáticas antes de su descubrimiento. En 2011, Neptuno completó su primera órbita de 165 años desde su descubrimiento en 1846. La Voyager 2 de la NASA es la única nave espacial que ha visitado Neptuno de cerca. Pasó volando en 1989 cuando salía del sistema solar.',
+        estructura: 'La estructura interna de Neptuno se parece a la de Urano: un núcleo rocoso cubierto por una costra helada, oculto bajo una atmósfera gruesa y espesa.​ Los dos tercios interiores de Neptuno se componen de una mezcla de roca fundida, agua, amoníaco líquido y metano. El tercio exterior es una mezcla de gas caliente compuesto de hidrógeno, helio, agua y metano. ',
+        rotacion: '16 hrs',
+        traslacion: '164 años',
+        radio: '24,622 km',
+        lunas: '14'
+    },
+]
+
+const rotacion = document.getElementById('rotacion')
+const traslacion = document.getElementById('traslacion')
+const radio = document.getElementById('radio')
+const luna = document.getElementById('luna')
+const name = document.getElementById('name')
+const info = document.getElementById('info')
+const img = document.getElementById('img')
+const estructuraButton = document.getElementById('estructuraButton')
+
+// function hola (){
+//     estructuraButton.onclick = console.log("hola")
+// }
+// let var1 = tierra()
+
+
+let tierra = () => {
+    name.innerText = planetas[0].nombre,
+    info.innerText = planetas[0].info,
+    rotacion.innerText = planetas[0].rotacion,
+    traslacion.innerText = planetas[0].traslacion
+    radio.innerText = planetas[0].radio,
+    luna.innerText = planetas[0].lunas,
+    img.src = '../resources/img/tierra.png'
+}
+let mercurio = () => {
+    name.innerText = planetas[1].nombre,
+    info.innerText = planetas[1].info,
+    rotacion.innerText = planetas[1].rotacion,
+    traslacion.innerText = planetas[1].traslacion
+    radio.innerText = planetas[1].radio,
+    luna.innerText = planetas[1].lunas,
+    img.src = '../resources/img/mercurio.png'
+    
+}
+let venus = () => {
+    name.innerText = planetas[2].nombre,
+    info.innerText = planetas[2].info,
+    rotacion.innerText = planetas[2].rotacion,
+    traslacion.innerText = planetas[2].traslacion
+    radio.innerText = planetas[2].radio,
+    luna.innerText = planetas[2].lunas,
+    img.src = '../resources/img/venus.png'
+}
+let marte = () => {
+    name.innerText = planetas[3].nombre,
+    info.innerText = planetas[3].info,
+    rotacion.innerText = planetas[3].rotacion,
+    traslacion.innerText = planetas[3].traslacion
+    radio.innerText = planetas[3].radio,
+    luna.innerText = planetas[3].lunas,
+    img.src = '../resources/img/marte.png'
+}
+let jupiter = () => {
+    name.innerText = planetas[4].nombre,
+    info.innerText = planetas[4].info,
+    rotacion.innerText = planetas[4].rotacion,
+    traslacion.innerText = planetas[4].traslacion
+    radio.innerText = planetas[4].radio,
+    luna.innerText = planetas[4].lunas
+}
+let saturno = () => {
+    name.innerText = planetas[5].nombre,
+    info.innerText = planetas[5].info,
+    rotacion.innerText = planetas[5].rotacion,
+    traslacion.innerText = planetas[5].traslacion
+    radio.innerText = planetas[5].radio,
+    luna.innerText = planetas[5].lunas,
+    img.src = '../resources/img/saturno.png'
+}
+let urano = () => {
+    name.innerText = planetas[6].nombre,
+    info.innerText = planetas[6].info,
+    rotacion.innerText = planetas[6].rotacion,
+    traslacion.innerText = planetas[6].traslacion
+    radio.innerText = planetas[6].radio,
+    luna.innerText = planetas[6].lunas,
+    img.src = '../resources/img/urano.png'
+}
+let neptuno = () => {
+    name.innerText = planetas[7].nombre,
+    info.innerText = planetas[7].info,
+    rotacion.innerText = planetas[7].rotacion,
+    traslacion.innerText = planetas[7].traslacion
+    radio.innerText = planetas[7].radio,
+    luna.innerText = planetas[7].lunas,
+    img.src = '../resources/img/neptuno.png'
+}
+
+
+
